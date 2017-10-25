060e527adf7fe72c07a4597dffddb6b4890af265
Ryan Balfanz <rbalfanz@gmail.com>
2017-10-25 00:58:16 -0700

    Synchronize the speed with Firebase

--- src/App.js
+++ src/App.js
@@ -2,6 +2,8 @@ import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
 
+import * as firebase from 'firebase';
+
 class App extends Component {
 
   constructor() {
@@ -9,6 +11,16 @@ class App extends Component {
     this.state = {
       speed: 10
     };
+  }
+
+  componentDidMount() {
+    const rootRef = firebase.database().ref().child('react');
+    const speedRef = rootRef.child('speed');
+    speedRef.on('value', snapshot => {
+      this.setState({
+        speed: snapshot.val()
+      });
+    });
   }
 
   render() {
