7d389ab20b7854e4794972dcc396db41f0f020c5
Ryan Balfanz <rbalfanz@gmail.com>
2017-10-25 00:31:44 -0700

    Initialize Firebase App

--- src/index.js
+++ src/index.js
@@ -4,5 +4,18 @@ import './index.css';
 import App from './App';
 import registerServiceWorker from './registerServiceWorker';
 
+import * as firebase from 'firebase';
+
+// Initialize Firebase
+var config = {
+    apiKey: "AIzaSyD29yfrssePfYIq3syJdS021wRg7v7oADU",
+    authDomain: "fir-react-starter-d0f78.firebaseapp.com",
+    databaseURL: "https://fir-react-starter-d0f78.firebaseio.com",
+    projectId: "fir-react-starter-d0f78",
+    storageBucket: "",
+    messagingSenderId: "57377973906"
+};
+firebase.initializeApp(config);
+
 ReactDOM.render(<App />, document.getElementById('root'));
 registerServiceWorker();
